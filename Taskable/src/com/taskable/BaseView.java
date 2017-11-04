e46249acf364c3463d1b8fbcddf13bbb01ba53e0
sarahchou <chou.s@husky.neu.edu>
2017-11-04 16:35:17 -0400

    Added baseview class

---
+++ Taskable/src/com/taskable/BaseView.java
@@ -0,0 +1,17 @@
+package com.taskable;
+
+/**
+ * Class for the base view of the Taskable app.
+ *
+ * BorderLayout
+ *    top nav
+ *    left project side
+ *    middle empty
+ * GridLayout
+ *    project list (from UserModel)
+ * TabLayout
+ *    when clicking on a button, for Tasks and Overview (or two smaller buttons)
+ */
+public class BaseView {
+
+}
