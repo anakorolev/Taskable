8c3770ab2026e4bbda64b3045779c11fa142689e
sarahchou <chou.s@husky.neu.edu>
2017-11-04 17:17:38 -0400

    Added the top three buttons to the baseView

--- Taskable/src/com/taskable/BaseView.java
+++ Taskable/src/com/taskable/BaseView.java
@@ -1,5 +1,9 @@
 package com.taskable;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 /**
  * Class for the base view of the Taskable app.
  *
@@ -13,5 +17,44 @@ package com.taskable;
  *    when clicking on a button, for Tasks and Overview (or two smaller buttons)
  */
 public class BaseView {
+
+  private static JPanel basePanel;
+  private static JPanel baseGridPanel;
+
+  //empty constructor
+  public BaseView() {
+    basePanel = new JPanel();
+    baseGridPanel = new JPanel();
+  }
+
+  public static void main(String[] args) {
+
+    JFrame frame = new JFrame("Taskable");
+
+    JButton members = new JButton("See Members"); //TODO add icon later
+    JButton addTask = new JButton("Add Tasks"); //TODO add icon later
+    JButton actions = new JButton("Actions"); //TODO add the actions for delete, complete, etc
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new BorderLayout());
+    buttonPanel.add(members, BorderLayout.LINE_START);
+    buttonPanel.add(addTask, BorderLayout.CENTER);
+    buttonPanel.add(actions, BorderLayout.LINE_END);
+
+
+
+    frame.setContentPane(new BaseView().basePanel);
+
+    basePanel.setLayout(new BorderLayout());
+    baseGridPanel.setLayout(new GridLayout());
+
+    basePanel.add(buttonPanel, BorderLayout.NORTH);
+    basePanel.add(baseGridPanel, BorderLayout.WEST);
+
+
+    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+  }
 
 }
